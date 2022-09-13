d85125dea38a1d79c11410ad6b19ea5d72aa2c87
Alihan <alihandegirmenciler@gmail.com>
2022-09-13 12:26:26 +0300

    update

--- components/Script.js
+++ components/Script.js
@@ -1,10 +1,13 @@
 
 async function filterByGroupName(GroupName) {
-    const JsonData = await fetch("components/Data.json")                        // Datayı fetch ile aldım.
+
+    const JsonData = await fetch("components/Data.json")  // Datayı fetch ile aldım.
         .then((response) => response.json())
-        .catch((error) => console.log(error));                                  // Hata olması durumunda bunu console'a yazdırdım.
-    var DataList = JsonData.filter(item => item.group === GroupName);           // JsonData'yı filter yaparak GroupName adı verdiğim parametreye eşitledim.
-    getGroups(DataList);                                                        // getGroups fonksiyonunu çağırarak DataList'i parametre olarak yolladım.
+        .catch((error) => console.log(error));  // Hata olması durumunda bunu console'a yazdırdım.
+
+    var DataList = JsonData.filter(item => item.group === GroupName);           // JsonData'yı filter yaparak GroupName adı verdiğim parametreye eşitledim. Burada grup adına eşit olup olmadığını kontrol ediyoruz
+    DataList.length > 1 ? getGroups(DataList) : console.log("Hatalı grup adı");    // Eğer Datalist boşsa hata veriyor, değilse getGroups fonksiyonunu çağırarak DataList'i parametre olarak yolladım.
+
 };
 
 function getGroups(arr) {
@@ -23,7 +26,7 @@ function getGroups(arr) {
     console.log(Result);                                                        //Sonucu konsola bastırıyoruz
 }
 
-filterByGroupName("SteelBlue");                                                 // Bu fonksiyon ile filtrelemek istediğimiz grup ismini giriyoruz
+filterByGroupName("Orcshid");                                                 // Bu fonksiyon ile filtrelemek istediğimiz grup ismini giriyoruz
 
 // Bilgi edindiğim kaynaklar
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/group
